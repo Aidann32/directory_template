9ba793da5299e2f3e7b3014382e479184d48bcbf
aliai <aliaidan02@gmail.com>
2024-08-27 19:52:25 +0500

    Windows check directory name fix

--- internal/utils/os_utils/windows.go
+++ internal/utils/os_utils/windows.go
@@ -63,8 +63,8 @@ func (u WindowsUtils) ValidateDirectoryName(name string) (err error) {
 		"CON", "PRN", "AUX", "NUL", "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9",
 		"LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9",
 	}
-	for _, name := range invalidNames {
-		if strings.EqualFold(name, name) {
+	for _, invalidName := range invalidNames {
+		if strings.EqualFold(name, invalidName) {
 			return errors.New("directory name is reserved")
 		}
 	}
